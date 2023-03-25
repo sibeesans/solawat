8cf9a068fdbb45ecc3b716194d18050ed2b39657
jaka1m <61670480+jaka1m@users.noreply.github.com>
2023-03-25 10:15:44 +0800

    Add files via upload

---
+++ ssh-ws/ssh-ws.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/jaka1m/project/main/ssh-ws/websocket.py
+chmod +x /usr/local/bin/ws-nontls
+
+# Installing Service
+cat > /etc/systemd/system/ws-nontls.service << END
+[Unit]
+Description=Python Proxy
+Documentation=https://t.me/sampiiiiu
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 2052
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-nontls
+systemctl restart ws-nontls
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/jaka1m/project/main/ssh-ws/ws-ovpn.py
+chmod +x /usr/local/bin/ws-ovpn
+
+# Installing Service
+cat > /etc/systemd/system/ws-ovpn.service << END
+[Unit]
+Description=Python Proxy
+Documentation=https://t.me/sampiiiiu
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpn 2086
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-ovpn
+systemctl restart ws-ovpn
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-tls https://raw.githubusercontent.com/jaka1m/project/main/ssh-ws/ws-tls
+chmod +x /usr/local/bin/ws-tls
+
+# Installing Service
+cat > /etc/systemd/system/ws-tls.service << END
+[Unit]
+Description=Python Proxy
+Documentation=https://geolstore.net
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 443
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-tls
+systemctl restart ws-tls
