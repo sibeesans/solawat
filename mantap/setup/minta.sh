926c556ef96469a21edc2b1f158f171ac7e1eab4
jaka1m <61670480+jaka1m@users.noreply.github.com>
2023-03-26 16:08:08 +0800

    Update minta.sh

--- mantap/setup/minta.sh
+++ mantap/setup/minta.sh
@@ -11,9 +11,8 @@ clear
 echo ""
 echo ""
 # // Setup
-echo -e "${GREEN}Downloading Setup!${NC}"
+echo -e "${GREEN}DOWNLOADING FILE SETUP!${NC}"
 sleep 3
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl && wget https://jaka1m.github.io/project/mantap/setup/setup.sh && chmod +x setup.sh && ./setup.sh
 rm -f /root/minta.sh > /dev/null 2>&1
 echo ""
-
