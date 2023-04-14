318cce4ead150edbc97792dcfe0f2e6521e443f9
jaka1m <61670480+jaka1m@users.noreply.github.com>
2023-04-14 19:10:38 +0800

    Update cf.sh

--- mantap/cf.sh
+++ mantap/cf.sh
@@ -35,9 +35,9 @@ echo ""
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 apt install jq curl -y >/dev/null 2>&1
-DOMAIN=gabutvpn.tech
+DOMAIN=lekong.me
 sub=$(</dev/urandom tr -dc a-x1-9 | head -c6 | tr -d '\r' | tr -d '\r\n')
-SUB_DOMAIN=${sub}.gabutvpn.tech
+SUB_DOMAIN=${sub}.lekong.me
 CF_ID=paoandest@gmail.com
 CF_KEY=1d158d0efc4eef787222cefff0b6d20981462
 set -euo pipefail
